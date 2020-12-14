45d5ec5e792694dee44c528f88d022e187f6c106
fatimah1155 <70664164+fatimah1155@users.noreply.github.com>
2020-12-14 11:50:19 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let درجة = 0
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+    درجة = input.compassHeading()
+    if (true) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    }
+})
